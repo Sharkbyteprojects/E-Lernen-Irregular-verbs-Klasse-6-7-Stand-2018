aa5b18b88385f472d0c635f45b4cd2955f3be253
SHARK <shark>
2019-01-27 11:38:32 +0100

    NEW

--- ECLIPSE/src/gui/GUI.java
+++ ECLIPSE/src/gui/GUI.java
@@ -46,7 +46,7 @@ public class GUI extends JFrame {
 		setResizable(false);
 		setTitle("ENGLISCH IRREGULAR VERBS");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 156);
+		setBounds(100, 100, 450, 194);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
