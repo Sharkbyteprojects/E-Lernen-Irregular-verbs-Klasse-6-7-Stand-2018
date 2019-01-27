8ebe73294b640a6978047aa7e0f67d15f5be305c
SHARK <shark>
2019-01-27 10:07:29 +0100

    RN

--- ECLIPSE/src/Main.java
+++ ECLIPSE/src/Main.java
@@ -2,15 +2,26 @@
 import java.io.*;
 
 public class Main {
-	int richtig = 0;
-	int gesamt = 0;
 	public static void main (String args[]) throws IOException {
-		String be;
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String was = "was";
+		String wer = "were";
+		int richtig = 0;
+		int gesamt = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("ENGLISCH UNREGELMAESSIGE VERBEN");
 		System.out.println("Nur kleinbuchstaben sind erlaubt!");
 		System.out.println("ENGLISCH UNREGELMAESSIGE VERBEN");
 		System.out.println("be 2.Form:");
-		be = input.readLine();
+		String be = br.readLine();
+		gesamt++;
+		if (be == was) {
+			System.out.println("Richtig, auch richtig ist were");
+			richtig++;
+		}else if(be == wer) {
+			System.out.println("Richtig, auch richtig ist was");
+			richtig++;
+		}else {
+			System.out.println("LEIDER FALSCH, richtig waere was oder were");
+		}
 	}
 }
