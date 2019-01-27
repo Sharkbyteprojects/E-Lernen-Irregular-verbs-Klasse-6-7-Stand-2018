9c4aabb65d20fa056bc3e5353f0422f9cd185a36
SHARK <shark>
2019-01-27 09:32:05 +0100

    ADD MAIN INPUT

--- ECLIPSE/src/Main.java
+++ ECLIPSE/src/Main.java
@@ -1,4 +1,16 @@
 
-public class Main {
+import java.io.*;
 
+public class Main {
+	int richtig = 0;
+	int gesamt = 0;
+	public static void main (String args[]) throws IOException {
+		String be;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("ENGLISCH UNREGELMAESSIGE VERBEN");
+		System.out.println("Nur kleinbuchstaben sind erlaubt!");
+		System.out.println("ENGLISCH UNREGELMAESSIGE VERBEN");
+		System.out.println("be 2.Form:");
+		be = input.readLine();
+	}
 }
